3d6846dece995b07949b58e5c992cd60200bb823
Kalpesh <kalpeshsantoki07@gmail.com>
2018-02-15 00:15:51 +0530

    git module1 added

---
+++ git-module1/src/main/java/com/hellowgit/module1/Module1Test.java
@@ -0,0 +1,7 @@
+package com.hellowgit.module1;
+
+public class Module1Test {
+    public void print(){
+        System.out.println("Hellow from Module1!");
+    }
+}
--- src/main/java/com/hellowgit/Launcher.java
+++ src/main/java/com/hellowgit/Launcher.java
@@ -1,7 +1,11 @@
 package com.hellowgit;
 
+import com.hellowgit.module1.Module1Test;
+
 public class Launcher {
     public static void main(String[] args){
         System.out.println("Hellow Git");
+
+        new Module1Test().print();
     }
 }
